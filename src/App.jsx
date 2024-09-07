0c30e62efa0457b283a4e5f0dd19704c7ff6646a
Sonu Singh <sonusinghrajput270@gmail.com>
2024-09-07 23:18:16 +0530

    Learn Redux Tool Kit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="max-w-screen-xl mx-auto">
+      <h1 className="text-5xl font-semibold bg-red-100 text-center my-5 py-3 font-mono">
+        Learn Redux
+      </h1>
+    </div>
+  );
+};
+
+export default App;
